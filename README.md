d296008ff3d721c3dc92ffdf1190c18910590886
El profe <al.sepulvedam@profesor.duoc.cl>
2025-10-07 16:31:39 -0300

    Fist commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# EJEMPLO COMPONENTES Y NAVEGACION 
+
+1. Agregar Librerias que se utilizarán
+   * Navigation Compose
+   * Iconos extendidos de material
---
+++ app/src/main/java/com/example/template_app_comp/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.template_app_comp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.template_app_comp.ui.theme.Template_app_compTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Template_app_compTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Template_app_compTheme {
+        Greeting("Android")
+    }
+}
